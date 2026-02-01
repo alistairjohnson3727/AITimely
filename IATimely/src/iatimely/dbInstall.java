700f635379453741d5aea426fca6d45e43568e0d
alistairjohnson3727 <alistair_johnson@s.thevillageschool.com>
2026-01-31 19:22:13 -0600

    created dbInstall

--- IATimely/src/iatimely/dbInstall.java
+++ IATimely/src/iatimely/dbInstall.java
@@ -4,11 +4,25 @@
  */
 package iatimely;
 
+
 /**
  *
  * @author alyj3
  */
 public class dbInstall
 {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    String dbName = "iaTimely";
+    dbAccess objAccess = new dbAccess();
+    objAccess.createDb(dbName);
+    String tableStatement =
+      "CREATE TABLE TestDatabase(year int, make varchar(55), model varchar(55))";
+    objAccess.createTable(tableStatement, dbName);
+  }
   
 }
