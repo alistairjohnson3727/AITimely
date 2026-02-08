5255fd551b614aa2ed344be33afc196f0e07b32a
alistairjohnson3727 <alistair_johnson@s.thevillageschool.com>
2026-02-08 10:00:53 -0600

    created my start GUI and another gui

---
+++ IATimely/src/iatimely/ChoiceSignupGUI.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iatimely;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author alyj3
+ */
+public class ChoiceSignupGUI extends JFrame implements ActionListener
+{
+  private JLabel titlePanel;
+  private JButton signupManButton;
+  private JButton signupEmpButton;
+  private JButton closeButton;
+  
+  public ChoiceSignupGUI()
+  {
+    super("Choose");
+    this.setBounds(100,100,200,100);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    signupEmpButton = new JButton("Employee");
+    signupEmpButton.addActionListener(this);
+    signupManButton = new JButton("Manager");
+    signupManButton.addActionListener(this);
+    titlePanel = new JLabel("Choose manager or employee sign up");
+    
+    this.add(titlePanel, BorderLayout.NORTH);
+    this.add(signupManButton,BorderLayout.CENTER);
+    this.add(signupEmpButton,BorderLayout.SOUTH);
+    this.setVisible(true);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent err)
+  {
+    String command = err.getActionCommand();
+    if (command.equals("Employee"))
+    {
+      //new InsertDisplay(this);
+    }
+    else if(command.equals("Manager"))
+    {
+     // new DatabaseDelete(this);
+    }
+  }
+}
---
+++ IATimely/src/iatimely/StartGUI.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iatimely;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author alyj3
+ */
+public class StartGUI extends JFrame implements ActionListener
+{
+  private JLabel TitleLabel;
+  private JLabel paragraphLabel;
+
+  
+  private JPanel buttonPanel;
+  private JPanel startPanel;
+  
+  private JButton loginButton;
+  private JButton signupButton;
+  private JButton closeButton;
+
+  public StartGUI()
+  {
+    super("Start page");
+    this.setBounds(100,100,800,500);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    String description = "";
+    TitleLabel = new JLabel("Welcome to AITimely");
+    paragraphLabel = new JLabel(description);
+    
+    loginButton = new JButton("Login");
+    loginButton.addActionListener(this);
+    signupButton = new JButton("Sign up");
+    signupButton.addActionListener(this);
+    closeButton = new JButton("Close");
+    closeButton.addActionListener(this);
+    
+    buttonPanel = new JPanel();
+    buttonPanel.add(loginButton);
+    buttonPanel.add(signupButton);
+    buttonPanel.add(closeButton);
+    
+    startPanel = new JPanel();
+    startPanel.add(paragraphLabel);
+    this.add(TitleLabel, BorderLayout.NORTH);
+    this.add(startPanel, BorderLayout.CENTER);
+    this.add(buttonPanel, BorderLayout.SOUTH);
+    this.setVisible(true);
+  }
+  @Override
+  public void actionPerformed(ActionEvent err)
+  {
+    String command = err.getActionCommand();
+    if (command.equals("Login"))
+    {
+      //new InsertDisplay(this);
+    }
+    else if(command.equals("Sign up"))
+    {
+      new ChoiceSignupGUI();
+    }
+    else if(command.equals("Close"))
+    {
+      this.dispose();
+    }
+  }
+  
+  public static void main(String[] args)
+  {
+    new StartGUI();
+  }
+}
