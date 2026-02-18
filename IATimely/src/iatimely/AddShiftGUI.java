73f025bcae89f64a43b3d590dd754a11a5c6134c
alistairjohnson3727 <alistair_johnson@s.thevillageschool.com>
2026-02-18 14:30:26 -0600

    Update AddShiftGUI.java

--- IATimely/src/iatimely/AddShiftGUI.java
+++ IATimely/src/iatimely/AddShiftGUI.java
@@ -4,11 +4,42 @@
  */
 package iatimely;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 /**
  *
  * @author alyj3
  */
-public class AddShiftGUI
+public class AddShiftGUI extends JFrame implements ActionListener
 {
-  
+
+  private JLabel descriptionLabel;
+  private JTextField descriptionField;
+  private JLabel dataLabel;
+  private JTextField dateField;
+  private JLabel EmployeeLabel;
+  private JTextField employeeIDField;
+  private JButton addButton;
+
+  public AddShiftGUI()
+  {
+
+    super("Add Shift");
+    this.setBounds(300, 300, 200, 100);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+    
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+  }
+
 }
