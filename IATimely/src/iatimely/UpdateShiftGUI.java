ca3ee8071a3ab590c03518cc499e85c7f10e9a2b
alistairjohnson3727 <alistair_johnson@s.thevillageschool.com>
2026-02-18 21:57:55 -0600

    added more stuff

--- IATimely/src/iatimely/UpdateShiftGUI.java
+++ IATimely/src/iatimely/UpdateShiftGUI.java
@@ -1,14 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package iatimely;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 /**
  *
  * @author alyj3
  */
-public class UpdateShiftGUI
+public class UpdateShiftGUI extends JFrame implements ActionListener
 {
-  
+  private JLabel title;
+  private JLabel newDescriptionLabel;
+  private JTextArea newDescriptionField;
+  private JLabel oldDateLabel;
+  private JTextField oldDateField;
+  private JLabel newDateLabel;
+  private JTextField newDateField;
+  private JLabel EmployeeLabel;
+  private JTextField employeeIDField;
+  private JButton updateButton;
+  private JPanel middlePanel;
+  public UpdateShiftGUI()
+  {
+
+    super("Add Shift");
+    this.setBounds(300, 300, 200, 100);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+    title = new JLabel("Add Shift");
+    newDescriptionLabel = new JLabel("New Description: ");
+    newDescriptionField = new JTextArea();
+    oldDateLabel = new JLabel("Old Date(YYYY-MM-DD): ");
+    oldDateField = new JTextField(10);
+    newDateLabel = new JLabel("New Date(YYYY-MM-DD): ");
+    newDateField = new JTextField(10);
+    EmployeeLabel = new JLabel("Employee ID: ");
+    employeeIDField = new JTextField(20);
+    updateButton = new JButton("Add Shift");
+    updateButton.addActionListener(this);
+    middlePanel = new JPanel();
+    
+    middlePanel.add(newDescriptionLabel);
+    middlePanel.add(newDescriptionField);
+    middlePanel.add(oldDateLabel);
+    middlePanel.add(oldDateField);
+    middlePanel.add(newDateLabel);
+    middlePanel.add(newDateField);
+    middlePanel.add(EmployeeLabel);
+    middlePanel.add(employeeIDField);
+    
+    this.add(title, BorderLayout.NORTH);
+    this.add(middlePanel,BorderLayout.CENTER);
+    this.add(updateButton,BorderLayout.SOUTH);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+  }
+
 }
--- IATimely/src/iatimely/ViewShiftGUI.java
+++ IATimely/src/iatimely/ViewShiftGUI.java
@@ -1,14 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package iatimely;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 /**
  *
  * @author alyj3
  */
-public class ViewShiftGUI
+public class ViewShiftGUI extends JFrame implements ActionListener
 {
-  
+  private JLabel title;
+  private JLabel dateLabel;
+  private JTextField dateField;
+  private JButton viewButton;
+  private JPanel middlePanel;
+  public ViewShiftGUI()
+  {
+
+    super("View Shift");
+    this.setBounds(300, 300, 200, 100);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+    title = new JLabel("Add Shift");
+    dateLabel = new JLabel("Date(YYYY-MM-DD): ");
+    dateField = new JTextField(10);
+    viewButton = new JButton("Add Shift");
+    viewButton.addActionListener(this);
+    middlePanel = new JPanel();
+    
+
+    middlePanel.add(dateLabel);
+    middlePanel.add(dateField);
+
+    
+    this.add(title, BorderLayout.NORTH);
+    this.add(middlePanel,BorderLayout.CENTER);
+    this.add(viewButton,BorderLayout.SOUTH);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+  }
+
 }
